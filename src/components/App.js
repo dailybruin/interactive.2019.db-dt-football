93f04d6ef196000bbd889ca4d2789d62a19816e4
Sarthak More <ssmore12@gmail.com>
2019-11-14 20:37:18 -0800

    bar graph almost done

--- src/components/App.js
+++ src/components/App.js
@@ -4,24 +4,25 @@ import { ArticleGrid } from "./ArticleGrid";
 import { PhotoGallery } from "./PhotoGallery";
 import { SubHeading } from "./SubHeading";
 import { Video } from "./Video";
-import { Navbar } from './Navbar'
-
+import { Navbar } from "./Navbar";
+import BarGraph from "./BarGraph/BarGraph";
 
 export class App extends React.Component {
-    render() {
-        return (
-            <>
-                {/* {this.props.visited || <LandingPage />} */}
-                <Navbar/>
-                <SubHeading text="Here's where your allegiance lies."/>
-                {/* <Choose/> */}
-                <SubHeading text="Here's how it all came together."/>
-                <PhotoGallery/>
-                <SubHeading text="Video Title."/>
-                <Video/>
-                <SubHeading text="Read more here."/>
-                <ArticleGrid />
-            </>
-        );
-    }
+  render() {
+    return (
+      <>
+        {/* {this.props.visited || <LandingPage />} */}
+        <Navbar />
+        <SubHeading text="Here's where your allegiance lies." />
+        {/* <Choose/> */}
+        <SubHeading text="Here's how it all came together." />
+        <PhotoGallery />
+        <SubHeading text="Video Title." />
+        <Video />
+        <SubHeading text="Read more here." />
+        <ArticleGrid />
+        <BarGraph />
+      </>
+    );
+  }
 }
