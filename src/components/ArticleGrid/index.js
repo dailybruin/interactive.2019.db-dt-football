779fecb132e10cc637d1a86040a3ceaff595ea7b
Tiffany Chandra <tiffanychandra@g.ucla.edu>
2019-11-15 13:49:37 -0800

    article grid

--- src/components/ArticleGrid/index.js
+++ src/components/ArticleGrid/index.js
@@ -1,77 +1,86 @@
 import React from "react";
 import { css } from "emotion";
-import { Section } from "./Section";
+import { SectionUCLA } from "./SectionUCLA";
+import { SectionUSC } from "./SectionUSC";
 import { colors } from "../Shared/colors";
 import { Header } from "./Header";
 
 export class ArticleGrid extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: [],
-            colorswitch: false
-        };
-        this.listenScrollEvent = this.listenScrollEvent.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      colorswitch: false
+    };
+    this.listenScrollEvent = this.listenScrollEvent.bind(this);
+  }
 
-    listenScrollEvent(e) {
-        if (window.scrollY > 130) {
-            this.setState({ colorswitch: true });
-        } else {
-            this.setState({ colorswitch: false });
-        }
+  listenScrollEvent(e) {
+    if (window.scrollY > 130) {
+      this.setState({ colorswitch: true });
+    } else {
+      this.setState({ colorswitch: false });
     }
+  }
 
-    componentWillMount() {
-        if (window) {
-            window.addEventListener("scroll", this.listenScrollEvent);
-        }
-        fetch(
-            "https://kerckhoff.dailybruin.com/api/packages/flatpages/interactive.2019.gradissue/"
-        )
-            .then(response => response.json())
-            .then(data => {
-                let sections = data.data["data.aml"].sections;
-                this.setState({
-                    data: sections
-                });
-            });
+  componentWillMount() {
+    if (window) {
+      window.addEventListener("scroll", this.listenScrollEvent);
     }
+    fetch(
+      "https://kerckhoff.dailybruin.com/api/packages/flatpages/interactive.2019.db-dt-football/"
+    )
+      .then(response => response.json())
+      .then(data => {
+        let sections = data.data["data.aml"].sections;
+        this.setState({
+          data: sections
+        });
+      });
+  }
 
-    componentWillUnmount() {
-        // This fixes the "leaky component" error
-        // Make sure to remove any event listeners you add!
-        if (window) {
-            window.removeEventListener("scroll", this.listenScrollEvent);
-        }
+  componentWillUnmount() {
+    // This fixes the "leaky component" error
+    // Make sure to remove any event listeners you add!
+    if (window) {
+      window.removeEventListener("scroll", this.listenScrollEvent);
     }
+  }
 
-    render() {
-        return (
-            <div
-                className={css`
-                    width: calc(100% - 20em);
-                    min-height: 90vh;
-                    text-align: center;
-                    padding: 10em;
-                    padding-top: 0.5em;
-                    border-top: 2vh solid white;
-                `}
-            >
-                {/* <Header
+  render() {
+    return (
+      <div
+        className={css`
+          width: calc(100% - 20em);
+          min-height: 90vh;
+          text-align: center;
+          padding: 10em;
+          padding-top: 0.5em;
+          border-top: 2vh solid white;
+        `}
+      >
+        {/* <Header
                     colorswitch={this.state.colorswitch}
                     sections={this.state.data.map(x => x.section)}
                 /> */}
-                {this.state.data.length && 
-                    <div className={css`
-                        display: grid;
-                        grid-template-columns: repeat(2,1fr);
-                    `}>
-                        <Section key={this.state.data[0].section} data={this.state.data[0]} />
-                        <Section key={this.state.data[1].section} data={this.state.data[1]} />
-                    </div>
-                }
-            </div>
-        );
-    }
+        {this.state.data.length && (
+          <div
+            className={css`
+              display: grid;
+              grid-template-columns: repeat(2, 1fr);
+            `}
+          >
+            <SectionUCLA
+              key={this.state.data[0].section}
+              data={this.state.data[0]}
+            />
+            <SectionUSC
+              key={this.state.data[1].section}
+              data={this.state.data[1]}
+            />
+          </div>
+        )}
+      </div>
+    );
+  }
 }
