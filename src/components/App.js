adacd2282e216ef29026a900a1b1ccf61b50b84d
tom22ger <thomasadrian@mac.com>
2019-11-15 22:50:29 -0800

    Added Timeline

--- src/components/App.js
+++ src/components/App.js
@@ -4,24 +4,73 @@ import { ArticleGrid } from "./ArticleGrid";
 import { PhotoGallery } from "./PhotoGallery";
 import { SubHeading } from "./SubHeading";
 import { Video } from "./Video";
-import { Navbar } from './Navbar'
+import { Navbar } from "./Navbar";
+import { Timeline } from "./Timeline";
 
+const timelineCards = [
+  {
+    img:
+      "https://dailybruin.com/images/2019/11/web.ns_.joebiden.KH_-640x466.jpg",
+    blurb:
+      "this thing happened and it was really cool thi thing happened and it was really cool this thing happened and it was really cool this thing really cool this thing happened and it was really cool this thing happened and it was really cool this thing happened and it was really cool this thing happened and it was really cool this thing happened and it was really cool this thing happened and it was really cool this thing happened and it was really cool happened",
+    year: "2015"
+  },
+  {
+    img:
+      "https://dailybruin.com/images/2019/11/web.ns_.joebiden.KH_-640x466.jpg",
+    blurb:
+      "this thing happened and it was really cool thi thing happened and it was really cool this thing happened and it was really cool this thing really cool this thing happened and it was really cool this thing happened and it was really cool this thing happened and it was really cool this thing happened and it was really cool this thing happened and it was really cool this thing happened and it was really cool this thing happened and it was really cool happened",
+    year: "2015"
+  },
+  {
+    img:
+      "https://dailybruin.com/images/2019/11/web.ns_.joebiden.KH_-640x466.jpg",
+    blurb:
+      "this thing happened and it was really cool thi thing happened and it was really cool this thing happened and it was really cool this thing really cool this thing happened and it was really cool this thing happened and it was really cool this thing happened and it was really cool this thing happened and it was really cool this thing happened and it was really cool this thing happened and it was really cool this thing happened and it was really cool happened",
+    year: "2015"
+  },
+  {
+    img:
+      "https://dailybruin.com/images/2019/11/web.ns_.joebiden.KH_-640x466.jpg",
+    blurb:
+      "this thing happened and it was really cool thi thing happened and it was really cool this thing happened and it was really cool this thing really cool this thing happened and it was really cool this thing happened and it was really cool this thing happened and it was really cool this thing happened and it was really cool this thing happened and it was really cool this thing happened and it was really cool this thing happened and it was really cool happened",
+    year: "2015"
+  },
+  {
+    img:
+      "https://dailybruin.com/images/2019/11/web.ns_.joebiden.KH_-640x466.jpg",
+    blurb:
+      "this thing happened and it was really cool thi thing happened and it was really cool this thing happened and it was really cool this thing really cool this thing happened and it was really cool this thing happened and it was really cool this thing happened and it was really cool this thing happened and it was really cool this thing happened and it was really cool this thing happened and it was really cool this thing happened and it was really cool happened",
+    year: "2015"
+  },
+  {
+    img:
+      "https://dailybruin.com/images/2019/11/web.ns_.joebiden.KH_-640x466.jpg",
+    blurb:
+      "this thing happened and it was really cool thi thing happened and it was really cool this thing happened and it was really cool this thing really cool this thing happened and it was really cool this thing happened and it was really cool this thing happened and it was really cool this thing happened and it was really cool this thing happened and it was really cool this thing happened and it was really cool this thing happened and it was really cool happened",
+    year: "2015"
+  }
+];
 
 export class App extends React.Component {
-    render() {
-        return (
-            <>
-                {/* {this.props.visited || <LandingPage />} */}
-                <Navbar/>
-                <SubHeading text="Here's where your allegiance lies."/>
-                {/* <Choose/> */}
-                <SubHeading text="Here's how it all came together."/>
-                <PhotoGallery/>
-                <SubHeading text="Video Title."/>
-                <Video/>
-                <SubHeading text="Read more here."/>
-                <ArticleGrid />
-            </>
-        );
-    }
+  render() {
+    return (
+      <>
+        {/* {this.props.visited || <LandingPage />} */}
+        <Navbar />
+        <SubHeading text="Here's where your allegiance lies." />
+        {/* <Choose/> */}
+        <SubHeading text="Here's how it all came together." />
+        <PhotoGallery />
+        <SubHeading text="Video Title." />
+        <Video />
+        <SubHeading text="Read more here." />
+        <Timeline
+          cards={timelineCards}
+          colors={["#CD6969", "#F7CD6A", "#83B8D7"]}
+        ></Timeline>
+        <ArticleGrid />
+      </>
+    );
+  }
 }
---
+++ src/components/Timeline/Card.jsx
@@ -0,0 +1,104 @@
+import React from "react";
+import { css } from "emotion";
+
+export class Card extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div
+        className={
+          (this.props.activenum == this.props.num ? "active " : "") +
+          css`
+            text-align: center;
+            width: 200px;
+            display: inline-block;
+            position: relative;
+            white-space: normal;
+            height: 440px;
+            vertical-align: baseline;
+            transition: width 300ms;
+
+            &.active {
+              width: 300px;
+            }
+          `
+        }
+      >
+        <div
+          className={
+            (this.props.activenum == this.props.num ? "active " : "") +
+            css`
+              background-color: ${this.props.color};
+              padding: 15px;
+              overflow: hidden;
+              height: 400px;
+              display: block;
+              vertical-align: bottom;
+
+              transition: all 500ms;
+              transform: translateY(360px);
+              &.active {
+                transform: translateY(0);
+              }
+            `
+          }
+        >
+          <img
+            src={this.props.img}
+            className={
+              (this.props.activenum == this.props.num ? "active " : "") +
+              css`
+                width: 100%;
+                height: 180px;
+                margin-bottom: 10px;
+                object-fit: cover;
+                opacity: 0;
+                transition: opacity 200ms;
+
+                &.active {
+                  opacity: 1;
+                }
+              `
+            }
+          ></img>
+          <div
+            className={css`
+              overflow-y: auto;
+              text-align: left;
+              height: 175px;
+            `}
+          >
+            <p
+              className={css`
+                color: #fff;
+                margin: 0;
+                font-size: 14px;
+              `}
+            >
+              {this.props.blurb}
+            </p>
+          </div>
+        </div>
+        <h1
+          className={
+            (this.props.activenum == this.props.num ? "active " : "") +
+            css`
+              margin: 0;
+              position: absolute;
+              bottom: 0;
+              left: 0;
+              right: 0;
+              background-color: #fff;
+              color: ${this.props.color};
+            `
+          }
+        >
+          {this.props.year}
+        </h1>
+      </div>
+    );
+  }
+}
